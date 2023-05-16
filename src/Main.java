40d368fe40076294ad2bc800bf47c0fc9f0647e8
josediaz449 <josediaz449@yahoo.com>
2023-05-16 14:54:50 -0400

    Main initiates UI.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    public Main() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new GameBoard());
+
+        setResizable(false);
+        pack();
+
+        setTitle("MovingCircleWithKeys.Circle");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Main();
+            ex.setVisible(true);
+        });
     }
 }
